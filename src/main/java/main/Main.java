8cc76072dea3ce707a534fac3cec94d2e0fcf17a
Anton Liseytsev <antonlis2@yandex.ru>
2018-04-19 09:31:40 +0300

    19.04.2018

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,9 @@
+package main;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-//import servlets.AllRequestsServlet;
+import servlets.AllRequestsServlet;
 
 /**
  * @author v.chibrikov
@@ -12,11 +14,10 @@ import org.eclipse.jetty.servlet.ServletHolder;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        //AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
-        //Новый коммит
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        //context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
         Server server = new Server(8080);
         server.setHandler(context);
